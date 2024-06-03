ba1a989a0ad63420bfb6b7d38d84a37734d8b4dc
LCRoque <lucas.roque@sptech.school>
2024-06-03 00:02:53 -0300

    Arrumar Jogo da Velha

--- web-data-viz/src/models/medidaModel.js
+++ web-data-viz/src/models/medidaModel.js
@@ -13,13 +13,23 @@ function buscarMedidasEmTempoReal() {
 }
 
 function buscarUltimasNoticias(limite_linhas) {
-    var instrucaoSql = `SELECT COUNT(*) AS quantidade_noticias FROM noticias`;
+    var instrucaoSql = `SELECT usuario.nome, COUNT(noticias.id) AS quantidade_posts 
+    FROM usuario
+    JOIN noticias ON usuario.id = noticias.fk_usuario
+    GROUP BY usuario.nome
+    ORDER BY quantidade_posts DESC
+    LIMIT ${limite_linhas};`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarNoticiasEmTempoReal() {
-    var instrucaoSql = `SELECT COUNT(*) AS quantidade_noticias FROM noticias;`;
+    var instrucaoSql = `SELECT usuario.nome, COUNT(noticias.id) AS quantidade_posts 
+    FROM usuario
+    JOIN noticias ON usuario.id = noticias.fk_usuario
+    GROUP BY usuario.nome
+    ORDER BY quantidade_posts DESC
+    LIMIT 3;`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
